98af370590d79b15dd2b575ec36e6a875c655708
jnajera06 <jose.najera06@gmail.com>
2021-05-01 22:52:29 +0000

    logger - morgan

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var http = require('http'), //This module provides the HTTP server functionaliti
 var router = express(); //We set our routing to be handled by Express
 var server = http.createServer(router); //This is where our server gets created
 
+router.use(logger('tiny'));
+
 router.use(express.static(path.resolve(__dirname, 'views'))); //We define the views folder as the one where all static content will be served
 router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
 router.use(express.json()); //We include support for JSON that is coming from the client
